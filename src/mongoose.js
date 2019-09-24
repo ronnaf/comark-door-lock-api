0428189daf7286ad6955e1427ee4101dd9656e9a
ronnamaeffirmo <ronnwiseley@gmail.com>
2019-09-24 15:16:54 +0800

    feat: connect to mlab instead of local db

--- src/mongoose.js
+++ src/mongoose.js
@@ -3,13 +3,13 @@ const logger = require('./logger');
 
 module.exports = function (app) {
   mongoose.connect(
-    app.get('mongodb'),
+    'mongodb://ronna-v2:JP8_uX3Pc6FQkBd@ds055722.mlab.com:55722/comark-door-lock-db',
     { useCreateIndex: true, useNewUrlParser: true }
   ).catch(err => {
     logger.error(err);
     process.exit(1);
   });
-  
+
   mongoose.Promise = global.Promise;
 
   app.set('mongooseClient', mongoose);
